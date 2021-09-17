25bf806b2516f05951ca711592c71bec008f048f
kishoreMittapelly <kishore.mittapelly@gmail.com>
2021-09-17 20:57:33 +0530

    video streaming changes

---
+++ src/test/java/com/kishore/streaming/SpringwebfluxVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishore.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxVideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
